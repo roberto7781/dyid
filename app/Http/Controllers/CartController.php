94ee2dd728cde1e5adb81a64b380cba61f2fdedc
roberto7781 <76868039+roberto7781@users.noreply.github.com>
2021-11-11 13:34:02 +0700

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,6 +8,11 @@ class CartController extends Controller
 {
     //
     public function addToCart(Request $request){
-        
+        $request->validate([
+            'inputQuantity' => 'required|min:0|not_in:0'
+
+        ]);
+
+        $data = $request->all();
     }
 }
