c483eea45f79e4b0bb8e4fb0f88a780a9dc7b1ee
roberto7781 <76868039+roberto7781@users.noreply.github.com>
2021-12-17 10:20:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,52 +32,55 @@ route::post('/user-login', [LoginController::class, 'processLogin'])->name('user
 route::post('/logout', [LoginController::class, 'logOut'])->name('logOut');
 
 // Dashboard
-route::get('/home', function(){
+route::get('/home', function () {
     return view('home');
 });
 
 
 // Category
-route::group(['middleware' => ['auth', 'role:Admin']], function(){
+route::group(['middleware' => ['auth', 'role:Admin']], function () {
     route::get('/category', [CategoryController::class, 'showCategoryView'])->name("categoryView");
 
     route::get('/addCategory', [CategoryController::class, 'showAddCategoryView'])->name("addCategoryView");
     route::post('/create-category', [CategoryController::class, 'addCategory'])->name('createCategory');
-    
+
     route::get('/editCategory{id}', [CategoryController::class, 'editCategoryView'])->name("editCategoryView");
     route::post('/updateCategory{id}', [CategoryController::class, 'updateCategory'])->name("updateCategory");
-    
+
     route::post('/deleteCategory{id}', [CategoryController::class, 'deleteCategory'])->name("deleteCategory");
 });
 
 
 
 // Product
-route::group(['middleware' => ['auth', 'role:Admin']], function(){
+route::group(['middleware' => ['auth', 'role:Admin']], function () {
     route::get('/product', [ProductController::class, 'showAdminProductView'])->name("adminProductView");
 
     route::get('/addProduct', [ProductController::class, 'showAddProductView'])->name("addProductView");
     route::post('/create-product', [ProductController::class, 'addProduct'])->name('createProduct');
-    
+
     route::get('/editProduct{id}', [ProductController::class, 'editProductView'])->name("editProductView");
     route::post('/updateProduct{id}', [ProductController::class, 'updateProduct'])->name("updateProduct");
-    
+
     route::post('/deleteProduct{id}', [ProductController::class, 'deleteProduct'])->name("deleteProduct");
 });
 
-route::get('/search',[ProductController::class, 'searchProducts'])->name('showSearchView');
+route::get('/search', [ProductController::class, 'searchProducts'])->name('showSearchView');
 route::get('/home', [ProductController::class, 'showProductView'])->name("productView");
 route::get('/productDetail', [ProductController::class, 'showProductDetailView'])->name('productDetailView');
 
 
 // Cart
-route::post('/addToCart{id}', [CartController::class, 'addToCart'])->name('addToCart');
-route::get('/viewCart', [CartController::class, 'showCartView'])->name('cartView');
 
-route::post('/deleteCart{id}', [CartController::class, 'deleteCart'])->name("deleteCart");
-route::get('/editCart{id}', [CartController::class, 'editCartView'])->name("editCartView");
-route::post('/updateCart{id}', [CartController::class, 'updateCart'])->name("updateCart");
+route::group(['middleware' => ['auth', 'role:Member']], function () {
+    route::post('/addToCart{id}', [CartController::class, 'addToCart'])->name('addToCart');
+    route::get('/viewCart', [CartController::class, 'showCartView'])->name('cartView');
 
-//Transaction
-route::get('/checkOut', [TransactionController::class, 'insertTransaction'])->name('checkOut');
-route::get('/transactionHistory', [TransactionController::class, 'showTransactionHistory'])->name('transactionHistoryView');
+    route::post('/deleteCart{id}', [CartController::class, 'deleteCart'])->name("deleteCart");
+    route::get('/editCart{id}', [CartController::class, 'editCartView'])->name("editCartView");
+    route::post('/updateCart{id}', [CartController::class, 'updateCart'])->name("updateCart");
+
+    //Transaction
+    route::get('/checkOut', [TransactionController::class, 'insertTransaction'])->name('checkOut');
+    route::get('/transactionHistory', [TransactionController::class, 'showTransactionHistory'])->name('transactionHistoryView');
+});
