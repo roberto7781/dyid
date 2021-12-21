236136384a5f9701a75eb5ea63aba72a442a9ab2
williamsunjaya46 <william.sunjaya.ws@gmail.com>
2021-12-22 00:39:54 +0700

    page not found update

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('pageLayout')
+
+@section('content')
+<div class="container mt-8" >
+    <div class="row">
+      <div class="col-sm-2">
+      </div>
+      <div class="col-sm-8 p-3" style="display: flex; justify-content: center; flex-direction: row;">
+        <img src="../storage/images/losticon.png" alt="lost-pic" style="height: 450px; margin: 5rem">
+      </div>
+      <div class="col-sm-2">
+      </div>
+    </div>
+  </div>
+
+
+</div>
+
+
+@endsection
